c30b60d9d030cebafb9292b8a12496b29eb3116c
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-22 14:34:48 +0600

    Pivot Binary Search

--- array/main.go
+++ array/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/alamin-mahamud/go-data-structures/array/rotation"
@@ -10,11 +11,18 @@ import (
 
 func main() {
 	d := flag.Int("d", 2, "rotate by this value")
+	s := flag.Int("s", -1, "search this Term")
 	flag.Parse()
 
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
 
 	if *d > len(a) {
+		log.Fatal("Rotation Point should not be greater than length")
+		os.Exit(1)
+	}
+
+	if *s == -1 {
+		log.Fatal("You must input the search Term")
 		os.Exit(1)
 	}
 
@@ -23,4 +31,5 @@ func main() {
 	// rotation.JugglingRotation(&a, *d)
 	rotation.ReversalAlgorithm(&a, *d)
 	fmt.Println(a)
+	fmt.Println(rotation.PivotBinarySearch(&a, *s))
 }
---
+++ array/rotation/pivot-binary-search.go
@@ -0,0 +1,70 @@
+/*
+Search an element in a sorted and rotated array
+*/
+package rotation
+
+func findPivot(a *[]int, low, high int) int {
+	// base cases
+	if high == low {
+		return -1
+	}
+
+	if high == low {
+		return low
+	}
+
+	midPoint := (low + high) / 2
+
+	if midPoint < high && (*a)[midPoint] > (*a)[midPoint+1] {
+		return midPoint
+	}
+
+	if midPoint > low && (*a)[midPoint] < (*a)[midPoint-1] {
+		return midPoint - 1
+	}
+
+	if (*a)[low] >= (*a)[midPoint] {
+		return findPivot(a, low, midPoint-1)
+	}
+
+	return findPivot(a, midPoint+1, high)
+
+}
+
+func BinarySearch(a *[]int, low, high, searchTerm int) int {
+
+	if high < low && high == low {
+		return -1
+	}
+
+	midPoint := (low + high) / 2
+
+	if (*a)[midPoint] == searchTerm {
+		return midPoint
+	}
+
+	if (*a)[midPoint] > searchTerm {
+		return BinarySearch(a, midPoint+1, high, searchTerm)
+	}
+
+	return BinarySearch(a, low, midPoint-1, searchTerm)
+}
+
+func PivotBinarySearch(a *[]int, searchTerm int) int {
+	pivot := findPivot(a, 0, len(*a)-1)
+
+	if pivot == -1 {
+		// in this case array is not rotated
+		BinarySearch(a, 0, len(*a)-1, searchTerm)
+	}
+
+	if (*a)[pivot] == searchTerm {
+		return pivot
+	}
+
+	if (*a)[pivot] < searchTerm {
+		return BinarySearch(a, 0, pivot, searchTerm)
+	}
+
+	return BinarySearch(a, pivot+1, len(*a)-1, searchTerm)
+}
